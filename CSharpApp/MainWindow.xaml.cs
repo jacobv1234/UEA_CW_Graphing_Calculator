234fc755cabef8d6b4119a46e9e85e78ba0c70f5
16dylayoun <16dylayoun@gmail.com>
2025-11-05 16:11:47 +0000

    adding plotting functionality

--- CSharpApp/MainWindow.xaml.cs
+++ CSharpApp/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Wpf;
 
 namespace CSharpApp
 {
@@ -51,6 +54,37 @@ namespace CSharpApp
         {
             string input_txt = Input_Text.Text;
 
+            try
+            {
+                string[] input_parts = input_txt.Split(';');
+
+                string equation = input_parts[0];
+                double.TryParse(input_parts[1], out double start);
+                double.TryParse(input_parts[2], out double stop);
+                double.TryParse(input_parts[3], out double step);
+
+                var plot_results = FSInterpreter.plot(equation, start, stop, step);
+
+                var points = plot_results.Select(innerlist => innerlist.First()).Select(pair => new DataPoint(pair.Item1, pair.Item2));
+
+                var model = new PlotModel { Title = equation };
+                var line_series = new LineSeries
+                {
+                    Title = equation,
+                    StrokeThickness = 2,
+                    MarkerType = MarkerType.Circle,
+                    MarkerSize = 2,
+                };
+
+                line_series.Points.AddRange(points);
+                model.Series.Add(line_series);
+
+                PlotView.Model = model;
+            }
+            catch (Exception ex)
+            {
+                Error_txt.Text = ex.Message;
+            }
         }
     }
 }
